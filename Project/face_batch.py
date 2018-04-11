715656a4da28f8a6349d3221a04335db782529e3
Darima Mylzenova <d.mylzenova@analysiscenter.com>
2018-04-11 22:11:13 +0300

    add project files and readme

---
+++ Project/face_batch.py
@@ -0,0 +1,142 @@
+"""Batch class for Celebrity faces project"""
+import sys
+
+import cv2 as cv
+import numpy as np
+from annoy import AnnoyIndex
+sys.path.append('..')
+from dataset import ImagesBatch, action, inbatch_parallel, any_action_failed, DatasetIndex, F
+
+class CelebrityBatch(ImagesBatch):
+    components = 'images', 'coordinates', 'embedding', 'neighbours', 'int_indices'
+
+    @action
+    def build_index(self, dst='my_index', n_trees=10):
+        """
+        """
+        n_dim = self.get(self.indices[0], 'embedding').shape[0]
+        n_items = len(self.indices)
+        print('n_dim: ', n_dim)
+        index = AnnoyIndex(n_dim)
+        for i in range(n_items):
+            index.add_item(i, self.get(self.indices[i], 'embedding'))
+        index.build(n_trees)
+        index.save(dst + '.ann')
+        return self
+
+    @action
+    @inbatch_parallel(init='indices', post='_assemble', target='threads', components='neighbours')
+    def find_nearest_neighbours(self, ix, src='my_index.ann', k_neighbours=6):
+        """
+        Finds k approximate nearest neighbours using
+        """
+        embd = self.get(ix, 'embedding')
+        saved_index = AnnoyIndex(embd.shape[0])
+        saved_index.load(src)
+        return saved_index.get_nns_by_vector(embd, k_neighbours)
+
+    @action
+    @inbatch_parallel(init='indices', post='_assemble', components='images')
+    def load_cv(self, ix, src, fmt='cv', **kwargs):
+        if fmt == 'cv':
+            return cv.imread(src + str(ix) + '.jpg')
+        else:
+            raise ValueError('fmt must be cv')
+
+    @action
+    @inbatch_parallel(init='images', post='_assemble', components='coordinates')
+    def detect_face(self, image):
+        """
+        Finds coordinates of the single face on the image using haarcascade from cv2.
+        Parameters
+        ----------
+        image : np.array
+            image with one face on it
+        
+        Returns
+        -------
+        bbox : tuple or list
+            face's bounding box in format (x, y, w, h)
+        """
+        # face_cascade = cv.CascadeClassifier('..//opencv//data//haarcascades//haarcascade_frontalface_default.xml')
+        face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        if len(faces) > 1:
+            print('detect_face has found more than one face')
+            raise ValueError
+        try:
+            bbox = faces[0]
+            return (bbox, )
+        except Exception as e:
+            print('failed faces=' , faces)
+            
+    @action
+    @inbatch_parallel(init='indices', post='_assemble', target='for', components='images')
+    def crop_from_bbox(self, ix, src='images', dst='images', to_rgb=False, component_coord='coordinates'):
+        """Crop area from an image using ``coordinates`` attribute
+        Parameters
+        ----------
+        src : str
+            data component's name
+        dst : str
+            the name of the component where the result will be recorded
+        component_coord : str
+            the name of the component with coordinates of the display with digits
+        Returns
+        -------
+        self
+        """
+        image = self.get(ix, src)
+        try:
+            x, y, width, height = self.get(ix, component_coord)
+        except Exception as e:
+            print(e, self.get(ix, component_coord))
+        dst_data = image[y:y+height, x:x+width]
+        if to_rgb:
+            dst_data = dst_data[:, :, ::-1]
+        return (dst_data, )
+
+    @action
+    @inbatch_parallel(init='images', target='for', post='_assemble', components='images')
+    def resize(self, image, *args, fmt='cv'):
+        if fmt == 'cv':
+            return (cv.resize(image, *args), )
+        else:
+            super().resize(image, *args)
+
+    @action
+    @inbatch_parallel(init='images', target='for', post='_assemble', components='images')
+    def to_rgb(self, image):
+        return (image[:, :, ::-1], )
+
+def load_func(data, fmt, components=None, *args, **kwargs):
+    """Writes the data for components to a dictionary of the form:
+    key : component's name
+    value : data for this component
+    Parameters
+    ----------
+    data : DataFrame
+        inputs data
+    fmt : strig
+        data format
+    components : list or str
+        the names of the components into which the data will be loaded.
+    Returns
+    -------
+    dict with keys - names of the compoents and values - data for these components.
+    """
+    # print('HERE')
+
+    _ = fmt, args, kwargs
+    _comp_dict = dict()
+    for comp in components:
+        if comp == 'int_index':
+            _comp_dict[comp] = data[comp].values.astype(int)
+        else:
+            data['tmp'] = data[data.columns[-2]].apply(lambda x: str(x).replace('[', '') \
+                                                                                              .replace(']', '') \
+                                                                                              .replace('', '')
+                                                                                              .split(' '))
+            _comp_dict[comp] = data['tmp'].apply(lambda x: np.array(list(map(float, [item for item in x if item != '']))))
+    return _comp_dict
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # celebrity_faces
+
+`Celebrity faces` is a project which supports a site where you can load your photo and get back photo of
+a celebrity who looks the most like you.
+
+## How does it works:
+Project/face_batch.py contains class CelebrityBatch which has methods for data preproccesing, Index creation and search for k approximate nearest neghbours. At the moment it utilizes spotify's [Annoy](https://github.com/spotify/annoy).
+
+CelebrityBatch class is inherited from [dataset's](https://github.com/analysiscenter/dataset) ImagesBatch 
+for flexible and easy looking workflows.
+
+An instance of CelebrityBatch represents data batch and stores components: `images`, `coordinates`, `embedding`, etc.
+
+Thus, image preproccesing pipeline looks like this:
+
+```python
+preprocess_ppl = (Pipeline()
+     .load_cv(src=src_images, fmt='cv', components='images')
+     .detect_face()
+     .crop_from_bbox()          
+     .resize(IMG_SHAPE, fmt='cv')
+     .to_rgb()
+     .to_pil()
+```
+
+To load a model from disk we create
+
+```python
+init_model_ppl = (Pipeline()
+                    .init_model('static', MyModel, model_name,
+                                config={'load' : {'path' : model_path, 'graph': model_name + '.meta',
+                                                  'checkpoint' : checkpoint_path}, 'build': False}))
+```
+
+And to create Index we merge two pipelines above and add predict function from Project/model.py and dump embeddings to the disk:
+
+```python
+predict_ppl = ((preprocess_ppl + init_model_ppl)
+                .predict_model(model_name, fetches="embeddings:0",
+                               feed_dict={'input:0' : B('images'), 'phase_train:0' : False},
+                               save_to=B('embedding'), mode='w')
+               .dump(dst='cropped_embeddings.csv', fmt='csv', mode='a', components=['embedding'], header=False)
+
+```
+
+This pipeline is lazy so we need to ask it to run:
+
+```python
+(predict_ppl << dset).run(BATCH_SIZE, n_epochs=1)
+```
+where dset is an instance of class dataset which supports indexing over current data.
+
+Once we have saved embeddings we are ready to build an AnnoyIndex and save it to the disk:
+```python
+preprocess_ppl = (Pipeline()
+     	.load(src='embeddings.csv', fmt='csv', components='embedding', index_col='file_name', post=load_func)
+       .build_index()
+```
+
+Then when we get new images we do the preproccessing and call a function to search for the neighbours using existing Index:
+
+```python
+my_workflow = ((preprocess_ppl)
+				.find_nearest_neighbours(src='my_index.ann', k_neighbours=6)
+				<< Dataset(index=FilesIndex(path=image_path)), batch_class=CelebrityBatch))
+```
+
+And the neighbours indices will be saves to the batch's component `neighbours`.
+
