27c2720b1a115d85b8ec610f9e899030457a499e
goltsman <goltsman@yandex-team.ru>
2018-05-09 16:46:23 +0300

    fix description of extension

--- our_annoy/lsh_index/setup.py
+++ our_annoy/lsh_index/setup.py
@@ -4,13 +4,13 @@ from Cython.Build import cythonize
 
 ext = Extension(
     "pylsh",
-    sources=["lsh.pyx", "lsh.cpp"],
+    sources=["lsh.pyx"],
     language="c++",
-    extra_compile_args=["-stdlib=libc++"],
+    extra_compile_args=["-std=c++11", "-std=gnu++11"],
 )
 
 setup(
     name="pylsh",
     ext_modules=cythonize(ext, language='c++'),
-
 )
+
