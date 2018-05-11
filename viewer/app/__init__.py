e075f350d0c569ce2672d40fa9593db2c9ea392b
goltsman <goltsman@yandex-team.ru>
2018-05-11 22:57:13 +0300

    Add ability to change upload_img_path

--- viewer/app/__init__.py
+++ viewer/app/__init__.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-import os
 from flask import Flask
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
-app = Flask(__name__)
+app = None
+photos = None
 
-photos = UploadSet('photos', IMAGES)
 
-app.config['SECRET_KEY'] = 'dfsdfvwn56vesvnw4fcyvhn5746v83w5'
-app.config['UPLOADED_PHOTOS_DEST'] = os.path.dirname(__file__) + '/static/uploaded_img'
-configure_uploads(app, photos)
+def init_app(uploaded_img_path):
+    global app, photos
+
+    app = Flask(__name__)
+    photos = UploadSet('photos', IMAGES)
+
+    app.config['SECRET_KEY'] = 'dfsdfvwn56vesvnw4fcyvhn5746v83w5'
+    app.config['UPLOADED_PHOTOS_DEST'] = uploaded_img_path
+    configure_uploads(app, photos)
+
+    # Тут к app привязываются все view_functions
+    from app import views
--- viewer/run.py
+++ viewer/run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
-from app import app
-from app import views
+import os
+import app
 
 
 def parse_args():
@@ -10,14 +10,16 @@ def parse_args():
     parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=2018)
     parser.add_argument("--debug", action="store_true", default=False)
+    parser.add_argument("--uploaded-img-path", type=str,
+                        default=os.path.dirname(__file__) + '/app/static/uploaded_img')
 
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-
-    app.run(host=args.host, port=args.port, debug=args.debug)
+    app.init_app(args.uploaded_img_path)
+    app.app.run(host=args.host, port=args.port, debug=args.debug)
 
 
 if __name__ == "__main__":
