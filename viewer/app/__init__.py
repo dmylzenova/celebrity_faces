652cf3550ab316102e13b0c0d439f5bc11565280
goltsman <goltsman@yandex-team.ru>
2018-05-11 21:30:48 +0300

    fix path

--- viewer/app/__init__.py
+++ viewer/app/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
@@ -7,5 +8,5 @@ app = Flask(__name__)
 photos = UploadSet('photos', IMAGES)
 
 app.config['SECRET_KEY'] = 'dfsdfvwn56vesvnw4fcyvhn5746v83w5'
-app.config['UPLOADED_PHOTOS_DEST'] = 'app/static/uploaded_img'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.dirname(__file__) + 'static/uploaded_img'
 configure_uploads(app, photos)
