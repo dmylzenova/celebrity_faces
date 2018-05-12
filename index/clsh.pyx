7eddb355fed32fd38a593cc94942792cc0d65268
Elena Zherdeva <helenzgerdeva@gmail.com>
2018-05-12 22:20:07 +0300

    fixed

--- index/clsh.pyx
+++ index/clsh.pyx
@@ -14,7 +14,7 @@ cdef extern from "lsh.h":
         LSH() except +
         LSH(int, int, int)
         void create_splits(cpp_vec_vec_double)
-        void add_to_table(c_embedding_type)
+        void add_to_table(int, cpp_vector_double)
         cpp_vector_int find_k_neighboors(int, cpp_vector_double)
         cpp_vector_int dummy_k_neighboors(int, int, cpp_vector_int, cpp_vec_vec_double,
                                                      cpp_vector_double)
@@ -26,8 +26,8 @@ cdef class PyLSH:
         self.thisptr = LSH(num_hash_tables, num_splits, dimension_size)
     def create_splits(self, points):
         return self.thisptr.create_splits(points)
-    def add_to_table(self, point):
-        return self.thisptr.add_to_table(point)
+    def add_to_table(self, int, cpp_vector_double):
+        return self.thisptr.add_to_table(int, cpp_vector_double)
     def find_k_neighboors(self, k, point):
         return self.thisptr.find_k_neighboors(k, point)
     def dummy_k_neighboors(self, k, index, indexes, embeddings, given_point):
