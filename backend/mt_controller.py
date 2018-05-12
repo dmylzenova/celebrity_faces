214dff797ce7bd365f3eb15d997242c4714dbb5a
goltsman <goltsman@yandex-team.ru>
2018-05-12 20:29:01 +0300

    small code improvements

--- backend/mt_controller.py
+++ backend/mt_controller.py
@@ -1,27 +1,22 @@
 import os
 import pandas as pd
 
-from face_batch import CelebrityBatch, load_func
-from dataset import FilesIndex, Dataset, Pipeline, V, B
+from dataset import FilesIndex, Dataset, Pipeline, B
 
-# sys.path.append('/model/')
+from face_batch import CelebrityBatch, load_func
 from model import MyModel
 
-class MtController:
-    def __init__(self):
-        model_path = os.path.join(os.getcwd(), 'model/')
-        model_name =  'model-20170512-110547'
-        checkpoint_path = 'model-20170512-110547.ckpt-250000'
-        index_path = 'data/150k_50t_index.ann'
-        int_mapping_file = 'data/int_indices_mapping.csv'
 
-        IMG_SHAPE = (160, 160)
-        K_NEIGHBOURS = 15
-	
-        self.src_images = 'cropped_photos/'
-        # self.meters_path = os.path.join(os.getcwd(), "default_meters")
-        # self.uploaded_files_path = os.path.join(os.getcwd(), "uploaded_meters")
-        # self.meters_filenames = sorted(os.listdir(self.meters_path))
+class MtController:
+    def __init__(self, k_neighbours=15, img_shape=(160, 160),
+                 cropped_photos_dir='cropped_photos',
+                 index_path='data/150k_50t_index.ann',
+                 model_name='model-20170512-110547',
+                 checkpoint_path='model-20170512-110547.ckpt-250000',
+                 int_mapping_file='data/int_indices_mapping.csv',
+                 model_path=os.path.join(os.getcwd(), 'model/')):
+
+        self.cropped_photos_dir = cropped_photos_dir
 
         self.find_neighbours_ppl = (Pipeline()
                                          .load(fmt='image', components='images')
@@ -29,17 +24,17 @@ class MtController:
                                          .to_cv(src='images')
                                          .detect_face()
                                          .crop_from_bbox()
-                                         .resize(IMG_SHAPE, fmt='cv')
+                                         .resize(img_shape, fmt='cv')
                                          .to_rgb()
                                          .init_variable('predicted_embeddings', init_on_each_run=0)
                                          .init_variable('indices', init_on_each_run=0)
                                          .init_model('static', MyModel, model_name,
-                                                     config={'load' : {'path' : model_path, 'graph': model_name + '.meta',
-                                                             'checkpoint' : checkpoint_path},  'build': False})
+                                                     config={'load': {'path': model_path, 'graph': model_name + '.meta',
+                                                             'checkpoint': checkpoint_path},  'build': False})
                                          .predict_model(model_name, fetches="embeddings:0",
-                                                        feed_dict={'input:0' : B('images'), 'phase_train:0' : False},
+                                                        feed_dict={'input:0': B('images'), 'phase_train:0': False},
                                                         save_to=B('embedding'), mode='w')
-                                         .find_nearest_neighbours(src=index_path, k_neighbours=K_NEIGHBOURS)
+                                         .find_nearest_neighbours(src=index_path, k_neighbours=k_neighbours)
                                    )
         self.indices_mapping = pd.read_csv(int_mapping_file, names=['file_name', 'int_index'], index_col='int_index')
         print('Initialized MtController...')
@@ -58,5 +53,5 @@ class MtController:
         batch = pred.next_batch(1)
         print('got next batch')
         knn = [self.indices_mapping.loc[neighbour_index, 'file_name'] for neighbour_index in batch.neighbours[0][:1]]
-        knn_files = [os.path.join(self.src_images, str(current) + '.jpg') for current in knn]
+        knn_files = [os.path.join(self.cropped_photos_dir, str(current) + '.jpg') for current in knn]
         return knn_files
