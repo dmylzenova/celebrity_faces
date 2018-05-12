777976f5cdb1be6866bd2756b5d34eb2196a9dc3
Ubuntu <ubuntu@ip-172-31-42-17.eu-central-1.compute.internal>
2018-05-12 14:22:46 +0000

    fixed clsh on server

--- our_annoy/lsh_index/clsh.pyx
+++ our_annoy/lsh_index/clsh.pyx
@@ -16,23 +16,22 @@ cdef extern from "lsh.h":
 
 ctypedef embedding_type c_embedding_type
 
-	cdef cppclass LSH:
-		LSH(int, int, int)
-		void create_splits(cpp_vec_vec_double)
-		void add_to_table(c_embedding_type)
-		cpp_vec_vec_double find_k_neighboors(int, int, cpp_vector_double)
+cdef extern from "lsh.h":
+    cdef cppclass LSH:
+        LSH() except +
+        LSH(int, int, int)
+        void create_splits(cpp_vec_vec_double)
+        void add_to_table(c_embedding_type)
+        cpp_vec_vec_double find_k_neighboors(int, int, cpp_vector_double)
 
 
 cdef class PyLSH:
-	cdef LSH thisptr
-	def __cinit__(self, num_hash_tables, num_splits, dimension_size):
-		self.thisptr = LSH(num_hash_tables, num_splits, dimension_size)
-
-	def create_splits(self, points):
-		return self.thisptr.create_splits(points)
-
-	def add_to_table(self, point):
-		return self.thisptr.add_to_table(point)
-
-	def find_k_neighboors(self, k, index, point):
-		return self.thisptr.find_k_neighboors(k, index, point)
+    cdef LSH thisptr
+    def __cinit__(self, num_hash_tables, num_splits, dimension_size):
+        self.thisptr = LSH(num_hash_tables, num_splits, dimension_size)
+    def create_splits(self, points):
+        return self.thisptr.create_splits(points)
+    def add_to_table(self, point):
+        return self.thisptr.add_to_table(point)
+    def find_k_neighboors(self, k, index, point):
+        return self.thisptr.find_k_neighboors(k, index, point)
