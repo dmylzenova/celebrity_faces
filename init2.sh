798f8ff8d3167d57fb4abd589b294e71a46ca4f8
dari <d.mylzenova@analysiscenter.ru>
2018-05-15 23:12:51 +0300

    upd link

--- init2.sh
+++ init2.sh
@@ -21,7 +21,7 @@ if [ ! -f model.zip ]; then
 fi
 
 if [ ! -f index.tar.gz ]; then
-    wget 'https://downloader.disk.yandex.ru/disk/36b06bd1a6059cf4129041960745bbac3b1358ee6e627c7dc91f5b778a94b1d7/5afb284f/hcn9DwIWb0_PkfTJnWMiaHGFEdVHxVXvMyxC7I_XlwJqJIawRyXEMfWFDKs3l2CMQ4Z9dkWJSL9HIjcL6bDV9Q%3D%3D?uid=0&filename=data.tar.gz&disposition=attachment&hash=%2B%2B6fwQGYQcslFHy7dzj3e%2B3J/uyYEquUqxV8CKH4W3U%3D&limit=0&content_type=application%2Fgzip&fsize=139173022&hid=cea20f2b9a830f0b67b8044389b769a5&media_type=compressed&tknv=v2' \
+    wget 'https://downloader.disk.yandex.ru/disk/5c1a7576278accfe4e6e8c398515eae357cada739b973d88804ee589206c7dce/5afb76fa/hcn9DwIWb0_PkfTJnWMiaHGFEdVHxVXvMyxC7I_XlwJqJIawRyXEMfWFDKs3l2CMQ4Z9dkWJSL9HIjcL6bDV9Q==?uid=0&filename=data.tar.gz&disposition=attachment&hash=++6fwQGYQcslFHy7dzj3e+3J/uyYEquUqxV8CKH4W3U=&limit=0&content_type=application/gzip&fsize=139173022&hid=cea20f2b9a830f0b67b8044389b769a5&media_type=compressed&tknv=v2' \
         -O index.tar.gz
     rm -rf index
     tar -xzvf index.tar.gz
