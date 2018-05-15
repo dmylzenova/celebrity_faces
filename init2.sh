6ed23d688a11a1b37a93cf33a2f1e7b5083d3bf3
goltsman <goltsman@yandex-team.ru>
2018-05-15 17:29:51 +0300

    fix sh file

--- init2.sh
+++ init2.sh
@@ -33,4 +33,4 @@ if [ ! -f cropped_photos.zip ]; then
     unzip cropped_photos.zip
 fi
 
-chmod +x $HOME/celebrity_faces/viewer/run.sh && $HOME/celebrity_faces/viewer/run.sh
+chmod +x $HOME/celebrity_faces/viewer/run.sh && $HOME/celebrity_faces/viewer/run.py
