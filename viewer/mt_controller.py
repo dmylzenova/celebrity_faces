cf6c90471f222c9961f0536d7d0aa68035863cf5
darimoon <d.mylzenova@analysiscenter.ru>
2018-05-16 15:03:38 +0300

    add dummy neighbours

--- viewer/mt_controller.py
+++ viewer/mt_controller.py
@@ -1,5 +1,7 @@
 import os
 import imageio
+
+import pandas as pd
 from dataset import FilesIndex, Dataset, Pipeline, B
 
 import pylsh
@@ -28,12 +30,14 @@ class MtController:
         assert self.index.fill_data_from_files(planes_path=self.planes_path,
                                                hash_tables_dir_path=self.hash_tables_dir_path,
                                                index_embedding_dict_path=self.index_embedding_dict_path)
-        print('LSH initialized')
-        a = '0.0194561 0.042469 -0.00241216 0.0404634 0.026206 0.0482399 0.0463853 -0.0908853 -0.0709574 -0.144772 0.011858 -0.114904 -0.0402228 0.100099 -0.0492029 -0.0944625 0.0665425 -0.0993312 0.00879607 -0.0796038 0.0261992 -0.181128 0.0627964 0.0843063 0.0743811 0.135361 0.112388 0.0272855 -0.0518486 -0.167158 -0.00187749 0.17207 -0.0719978 0.0496791 -0.0274812 0.0342841 0.151937 -0.124779 0.0700361 0.0733923 -0.0117331 -0.00160149 0.00506811 -0.0423161 -0.0740804 0.173151 0.0639334 0.0221061 0.0135171 -0.0809277 0.0553787 0.0043606 0.0919013 0.0685305 0.0240654 0.300156 0.0460886 -0.159905 0.168061 0.0932426 -0.0691807 0.0555146 0.0708056 0.0191332 -0.057153 0.0442914 0.118729 -0.0329558 0.0731806 0.102099 0.016923 0.133432 0.0202205 0.0725665 -0.117047 0.0847481 0.00099859 -0.0113888 -0.140756 -0.121368 -0.035942 0.0147354 0.00134443 0.0327282 0.126786 -0.0213281 0.0659621 -0.172813 0.112995 0.0858302 0.0137467 -0.101607 -0.0304303 -0.0391461 -0.0430552 0.185863 0.120424 -0.0756769 0.0509059 0.0491351 -0.0448453 -0.0902059 0.102599 -0.0472706 0.0404969 -0.026239 -0.162932 0.187223 0.0434553 -0.0994055 -0.0217782 -0.0192051 0.0020202 -0.0202612 0.0391378 0.113883 -0.0301008 0.104942 -0.0194257 -0.108668 0.0811891 -0.130431 0.0783285 0.0330896 0.00261154 -0.0231748 0.108669 -0.142922'
-        if len(self.index.find_k_neighbors(5, list(map(float, a.split(' '))))) == 0:
-            print('EMPTY INDEX')
-        else:
-            print('Successfully loaded LSH')
+        print('Successfully loaded LSH')
+
+        embeddings_data = pd.read_table(self.index_embedding_dict_path.decode('UTF-8'), names=['embd'])
+        embeddings_data['file_index'] = embeddings_data.embd.apply(lambda x: int(str(x)[:x.find(' ')]))
+        embeddings_data['embeddings'] = embeddings_data.embd.apply(lambda x: list(map(float, x[x.find(' ') + 1:-1].split(' '))))
+        self.file_indices = embeddings_data.file_index.values.tolist()
+        self.saved_embeddings = embeddings_data.embeddings.values.tolist()
+        print('Loaded embeddings')
 
         self.find_neighbours_ppl = (Pipeline()
                                          .load(fmt='image', components='images')
@@ -54,27 +58,34 @@ class MtController:
                                          .find_nearest_neighbours(self.index, k_neighbours=k_neighbours, use_pylsh=True)
                                    )
 
-        print('Initialized MtController...')
+        print('Ready!')
 
     def build_ds(self, path):
         print('BUILDING DATASET')
         return Dataset(index=FilesIndex(path=path), batch_class=face_batch.CelebrityBatch)
 
-    def get_inference(self, dir_path, name, images_count=1):
+    def get_inference(self, dir_path, name, return_dummy=False, images_count=1):
         print('GET_INFERENCE CALLED')
         src = dir_path + name
-        imageio.imread(src)
-
         print('here is what i got ', src)
+
         dset = self.build_ds(src)
         print('dataset has been built', dset.indices)
+
         pred = self.find_neighbours_ppl << dset
         print('created pred')
+
         batch = pred.next_batch(1)
         print('got next batch')
-        cropped_image = batch.images[0]
+
         dst = name + '_cropped.png'
-        imageio.imsave(dir_path + dst, cropped_image)
+        imageio.imsave(dir_path + dst, batch.images[0])
         print('saved cropped image to ', dst)
+
         knn_files = [str(current) + '.jpg' for current in batch.neighbours[0][:images_count]]
-        return dst, knn_files
+
+        result = dict(dst=dst, knn=knn_files)
+        if return_dummy:
+            result['dummy_knn'] = self.index.dummy_k_neighbors(images_count, self.file_indices, \
+                                                               self.saved_embeddings, batch.embedding[0])
+        return result
